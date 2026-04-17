118b9ce61976a0b8756b2fc477586f17560bf3f5
James Johnson <giganano9@gmail.com>
2026-04-16 22:40:09 -0400

    dev: setup.py made more systematic

--- setup.py
+++ setup.py
@@ -6,33 +6,69 @@
 # at https://github.com/giganano/cribbagehelper.git
 
 from setuptools import setup, Extension
+import glob
+import json
 import os
 if os.name != "posix": raise OSError("""\
 Sorry, Windows is not supported. We recommend using the Windows Subsystem for \
 Linux as a solution.""")
 
+COMPILER_DIRECTIVES = {
+	"embedsignature": True,
+	"embedsignature.format": "python"
+}
+
+
+class build:
+
+	r"""
+	A class providing functions for compiling cribbagehelper.
+	"""
+
+	@staticmethod
+	def extensions(metadatafile = "extensions.json"):
+		r"""
+		Create the extensions to compile with Cython.
+		The extension names, and the directories where C headers and source
+		files can be found are stored in a JSON mapping in extensions.json.
+		"""
+		extensions = []
+		metadata = json.load(open(metadatafile, 'r'))
+		for ext in metadata.keys():
+			cython_src_file = "%s.pyx" % (ext.replace('.', '/'))
+			c_src_files = []
+			for item in metadata[ext]["source_directories"]:
+				if item.endswith(".c"):
+					c_src_files.append(item)
+				else:
+					c_src_files.extend(glob.glob("%s/*.c" % (item)))
+			extensions.append(
+				Extension(
+					ext,
+					[cython_src_file] + c_src_files,
+					include_dirs = metadata[ext]["include_directories"]
+					)
+				)
+		return extensions
+
+
+	@staticmethod
+	def packages(path = "./cribbagehelper"):
+		r"""
+		Determine which directories should be treated as packages based on the
+		presence or lack of an __init__.py file.
+		"""
+		pkgs = []
+		for root, dirs, files in os.walk(path):
+			if "__init__.py" in files:
+				pkgs.append(root[2:].replace('/', '.'))
+			else: pass
+		return pkgs
+
+
 if __name__ == "__main__":
-	compilerdirectives = {"embedsignature": True}
-	extensions = [
-		Extension(
-			"cribbagehelper.core.card.card",
-			["cribbagehelper/core/card/card.pyx"],
-			include_dirs = ["cribbagehelper/core/card/src"]
-		),
-		Extension(
-			"cribbagehelper.core.hand.hand",
-			["cribbagehelper/core/hand/hand.pyx",
-			"cribbagehelper/core/hand/src/hand.c",
-			"cribbagehelper/scorehand/src/fifteens.c",
-			"cribbagehelper/scorehand/src/flush.c",
-			"cribbagehelper/scorehand/src/knobs.c",
-			"cribbagehelper/scorehand/src/pairs.c",
-			"cribbagehelper/scorehand/src/runs.c",
-			"cribbagehelper/scorehand/src/subsets.c"],
-			include_dirs = [
-				"cribbagehelper/core/hand/src",
-				"cribbagehelper/core/card/src",
-				"cribbagehelper/scorehand/src"],
-			)
-	]
-	setup(ext_modules = extensions)
+	setup(
+		ext_modules = build.extensions(),
+		packages = build.packages(),
+		compiler_directives = COMPILER_DIRECTIVES
+		)
